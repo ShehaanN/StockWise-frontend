e6695b2aaf05f362a867364fbeae06e850a31e28
ShehaanN <shehannadeesha2580@gmail.com>
2025-09-26 17:56:37 +0530

    initialize React project with Vite, Tailwind CSS, and ESLint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>Product Inventory</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
