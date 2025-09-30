4614be74c7a34864eb3a82e89bc5d71d94d72e4d
ShehaanN <shehannadeesha2580@gmail.com>
2025-09-30 16:37:23 +0530

    Created Sidebar component for navigation.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Sidebar = ({ activeItem }) => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo">📦 StockWise</div>
+      <ul className="sidebar-nav">
+        <li>
+          <Link
+            to="/products"
+            className={activeItem === "products" ? "active" : ""}
+          >
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/add-product"
+            className={activeItem === "addProduct" ? "active" : ""}
+          >
+            Add Product
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
